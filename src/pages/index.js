d38fdf9ccf19c1e2bd4b4e8382d06adffcba685d
noamsabb <sabban.noam@gmail.com>
2023-08-31 10:18:20 +0300

    fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,15 +30,6 @@ function HomepageHeader() {
           </p>
         </div>
         </div>
-
-
-        {/* <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div> */}
       </div>
     </header>
   );
